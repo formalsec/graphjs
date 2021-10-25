470e02409e59508312b84c31d57613cc56c0ca2f
Tiago Brito <tiago.de.oliveira.brito@tecnico.ulisboa.pt>
2021-10-25 16:28:52 +0100

    Saved version without multiple pdg objects

--- queries/query.py
+++ queries/query.py
@@ -4,40 +4,177 @@ def find_sink_function_calls(session, sink):
 	sink_calls = []
 	with session.begin_transaction() as tx:
 		# QUERY 1
-		# get statement holding the call to a sink
+		# get (function, sink statement) pair that holds the call to a sink
 		query = f"""
 			MATCH
-				(stmt:VariableDeclarator)-[init:AST]-(c:CallExpression)-[callee:AST]->(e:Identifier)
+				(f:FunctionDeclaration)-[:AST*1..]-(stmt:VariableDeclarator)-[init:AST]-(c:CallExpression)-[callee:AST]->(e:Identifier)
 			WHERE 
 				init.RelationType = 'init' AND
 				callee.RelationType = 'callee' AND
 				e.IdentifierName = '{sink}'
 			RETURN *
 		"""
-		results= tx.run(query)
+		results = tx.run(query)
 		for record in results:
-			sink_calls.append(record['stmt'])
+			sink_calls.append({
+				'function': record['f'],
+				'sink': record['stmt'],
+			})
 	
 	return sink_calls
 
 
-# def find_pdg_dependencies(session, decl):
-# 	dep_ids = []
-# 	identifier = decl.get('IdentifierName')
-# 	with session.begin_transaction() as tx:
-# 		# QUERY 2
-# 		# get all objects that reach a variable declaration with specific name
-# 		query = f"""
-# 			MATCH
-# 				(d:VariableDeclarator)-[:PDG*1..]->(x)
-# 			WHERE d.IdentifierName = '{identifier}'
-# 			RETURN *
-# 		"""
-# 		results= tx.run(query)
-# 		for record in results:
-# 			dep_ids.append(record['x'])
+def find_source_objects_and_variables(session):
+	params = []
+	with session.begin_transaction() as tx:
+		# QUERY 2
+		# get (function, parameter) pairs that we consider source
+		query = f"""
+			MATCH
+				(f:FunctionDeclaration)-[param:AST]-(p:Identifier)
+			WHERE 
+				param.RelationType = 'param'
+			RETURN *
+		"""
+		results = tx.run(query)
+		for record in results:
+			params.append({
+				'function': record['f'],
+				'param': record['p']
+			})
 
-# 	return dep_ids
+	param_objects = []
+	for param in params:
+		p_function = param['function'].get('Id')
+		p = param['param'].get('IdentifierName')
+		with session.begin_transaction() as tx:
+			# QUERY 3
+			# get (function, pdg_object) pairs that we consider source
+			query = f"""
+				MATCH
+					(f:FunctionDeclaration)-[c:PDG]->(obj:PDG_OBJECT)
+				WHERE
+					f.Id = '{p_function}' AND
+					c.RelationType = 'CREATE' AND
+					c.IdentifierName = '{p}'
+				RETURN *
+			"""
+			results = tx.run(query)
+					
+			if results.peek():
+				for record in results:
+					param_objects.append({
+						'function': record['f'],
+						'param': record['obj']
+					})
+			else:
+				param_objects.append(param)
+	
+	return param_objects
+
+
+# Check if there is a PDG Write to a source
+def check_if_exists_pdg_write_to_source(session, source, node):
+	source_id = source.get('Id')
+	node_id = node.get('Id')
+	with session.begin_transaction() as tx:
+		# QUERY 3
+		# get (function, parameter) pairs that we consider source
+		query = f"""
+			MATCH (node)-[pdg_edge:PDG]->(source)
+			WHERE 
+				node.Id = '{node_id}' AND
+				source.Id = '{source_id}' AND
+				pdg_edge.RelationType = 'WRITE'
+			RETURN *
+		"""
+		results = tx.run(query)
+		
+		return bool(results.peek())
+
+
+
+def check_if_tainted(session, sources, node, func):
+	for s in sources:
+		source_id = s['param'].get('Id')
+		node_id = node.get('Id')
+
+		# Check if node is also source
+		if s['function'].get('Id') == func.get('Id') and source_id == node_id:
+			return True
+		else:check_if_exists_pdg_write_to_source(session, s['param'], node)
+
+	return False
+
+
+def find_var_deps(session, sources, sink_call):
+	func = sink_call['function']
+
+	deps_queue = [ sink_call['sink'].get('Id') ]
+	var_deps = []
+
+	while len(deps_queue) > 0:
+		sink_id = deps_queue.pop()
+		print("SINK: ", sink_id)
+		with session.begin_transaction() as tx:
+			# QUERY 3
+			# get (function, parameter) pairs that we consider source
+			query = f"""
+				MATCH
+					(x)-[pdg_edge:PDG]->(sink_call)
+				WHERE 
+					sink_call.Id = '{sink_id}' AND
+					pdg_edge.RelationType = 'VAR'
+				RETURN *
+			"""
+			results = tx.run(query)
+			for record in results:
+				print(record['x'].get('Id'), record['x'].get('Type'))
+				deps_queue.append(record['x'].get('Id'))
+				var_deps.append(record['x'])
+
+	return var_deps
+				# print(check_if_tainted(sources, record['pdg_edge'], record['x'], func))
+
+
+def find_pdg_dependencies(session, sources, sinks):
+	for sink_call in sinks:
+		func = sink_call['function']
+		var_deps = find_var_deps(session, sources, sink_call)
+		for node in var_deps:
+			check_if_tainted(session, sources, node, func)
+		
+
+def find_pdg_paths(session, sources, sinks):
+	tainted_paths = []
+
+	for source in sources:
+		source_func = source['function'].get('Id')
+		source_id = source['param'].get('Id')
+
+		for sink in sinks:
+			sink_func = sink['function'].get('Id')
+			sink_id = sink['sink'].get('Id')
+
+			if source_func == sink_func:
+				print("Testing", source_id, sink_id)
+				with session.begin_transaction() as tx:
+					# QUERY 3
+					# get (function, parameter) pairs that we consider source
+					query = f"""
+						MATCH
+							(source)-[:PDG*1..]->(sink)
+						WHERE 
+							source.Id = '{source_id}' AND
+							sink.Id = '{sink_id}'
+						RETURN *
+					"""
+					results = tx.run(query)
+
+					if results.peek():
+						tainted_paths.append((source_id, sink_id))
+
+	return tainted_paths
 
 
 NEO4J_CONN_STRING="bolt://127.0.0.1:7687"
@@ -45,26 +182,8 @@ NEO4J_CONN_STRING="bolt://127.0.0.1:7687"
 neo_driver = GraphDatabase.driver(NEO4J_CONN_STRING, auth=('', ''))
 with neo_driver.session() as session:
 	calls = find_sink_function_calls(session, 'eval')
-	print(calls)
-	# for c in calls:
-	# 	deps = {
-	# 		'VariableDeclarator': [],
-	# 		'PDG_OBJECT': [],
-	# 		'FunctionDeclaration': []
-	# 	}
-
-	# 	direct_deps = find_pdg_dependencies(session, c)
-	# 	for d in direct_deps:
-	# 		# print(d)
-	# 		d_type = d.get('Type')
-	# 		if d_type in deps:
-	# 			deps[d_type].append(d)
-
-	# 	print(deps)
-		
-
-# MATCH (c:CallExpression)-[:AST {RelationType: 'callee'}]->(e:Identifier {IdentifierName: 'eval'}),
-# (call_decl:VariableDeclarator)-[:AST {RelationType: 'init'}]-(c:CallExpression)-[:AST {RelationType: 'arg'}]->(v_call),
-# (v_decl:VariableDeclarator)<-[:PDG]-(call_decl)
-# WHERE v_decl.IdentifierName = v_call.IdentifierName
-# RETURN *
+	# print(calls)
+	params = find_source_objects_and_variables(session)
+	# print(params)
+	print(find_pdg_paths(session, params, calls))
+	#find_pdg_dependencies(session, params, calls)
