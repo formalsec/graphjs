36b8aae150c67a5f7164c576fca705eabade04b4
Tiago Brito <tiago.de.oliveira.brito@tecnico.ulisboa.pt>
2022-07-15 16:02:25 +0100

    Fixed query for example-0

--- queries/query.py
+++ queries/query.py
@@ -31,47 +31,20 @@ def find_source_objects_and_variables(session):
 		# get (function, parameter) pairs that we consider source
 		query = f"""
 			MATCH
-				(f:FunctionExpression)-[param:AST]-(p:Identifier)
+				(f:FunctionExpression)-[param:AST]-(p:Identifier)-[create:PDG]->(obj:PDG_OBJECT)
 			WHERE
-				param.RelationType = 'param'
+				param.RelationType = 'param' AND
+				create.IdentifierName = p.IdentifierName
 			RETURN *
 		"""
 		results = tx.run(query)
 		for record in results:
 			params.append({
 				'function': record['f'],
-				'param': record['p']
+				'param': record['p'],
+				'param_obj': record['obj']
 			})
-
-	param_objects = []
-	for param in params:
-		p_function = param['function'].get('Id')
-		p = param['param'].get('IdentifierName')
-		with session.begin_transaction() as tx:
-			# QUERY 3
-			# get (function, pdg_object) pairs that we consider source
-			query = f"""
-				MATCH
-					(f:FunctionExpression)-[c:PDG]->(obj:PDG_OBJECT)
-				WHERE
-					f.Id = '{p_function}' AND
-					c.RelationType = 'CREATE' AND
-					c.IdentifierName = '{p}'
-				RETURN *
-			"""
-			results = tx.run(query)
-
-			if results.peek():
-				for record in results:
-					param_objects.append({
-						'function': record['f'],
-						'param_obj': record['obj'],
-						'param': param['param'],
-					})
-			else:
-				param_objects.append(param)
-
-	return param_objects
+	return params
 
 
 def find_pdg_paths(session, sources, sinks):
@@ -79,8 +52,7 @@ def find_pdg_paths(session, sources, sinks):
 
 	for source in sources:
 		source_func = source['function'].get('Id')
-		source_param_id = source['param'].get('Id')
-		source_param_obj_id = source['param_obj'].get('Id')
+		source_obj_id = source['param_obj'].get('Id')
 
 		for sink in sinks:
 			sink_func = sink['function'].get('Id')
@@ -88,41 +60,24 @@ def find_pdg_paths(session, sources, sinks):
 
 			if source_func == sink_func:
 				print("Testing path between:")
-				print("\tsource - ", source_param_id, ", and sink - ", sink_id)
+				print("\tsource - ", source_obj_id, ", and sink - ", sink_id)
 				with session.begin_transaction() as tx:
 					# QUERY 3
 					# get (function, parameter) pairs that we consider source
 					query = f"""
 						MATCH
-							(source)<-[:AST]-(f:FunctionExpression)-[:PDG*1..]->(sink)
+							(f:FunctionExpression)-[:AST]->(param)-[create:PDG]->(source)-[:PDG*1..]->(sink)
 						WHERE
 							f.Id = '{source_func}' AND
-							source.Id = '{source_param_id}' AND
+							create.RelationType = 'CREATE' AND
+							source.Id = '{source_obj_id}' AND
 							sink.Id = '{sink_id}'
 						RETURN *
 					"""
 					results = tx.run(query)
 
 					if results.peek():
-						tainted_paths.append((source_param_id, sink_id))
-
-				print("\tsource - ", source_param_obj_id, ", and sink - ", sink_id)
-				with session.begin_transaction() as tx:
-					# QUERY 3
-					# get (function, parameter) pairs that we consider source
-					query = f"""
-						MATCH
-							(source)<-[:AST]-(f:FunctionExpression)-[:PDG*1..]->(sink)
-						WHERE
-							f.Id = '{source_func}' AND
-							source.Id = '{source_param_obj_id}' AND
-							sink.Id = '{sink_id}'
-						RETURN *
-					"""
-					results = tx.run(query)
-
-					if results.peek():
-						tainted_paths.append((source_param_obj_id, sink_id))
+						tainted_paths.append((source_obj_id, sink_id))
 
 	return tainted_paths
 
@@ -132,9 +87,13 @@ NEO4J_CONN_STRING="bolt://127.0.0.1:7687"
 neo_driver = GraphDatabase.driver(NEO4J_CONN_STRING, auth=('', ''))
 with neo_driver.session() as session:
 	calls = find_sink_function_calls(session, 'eval')
-	# print(calls)
+	# for c in calls:
+	# 	print(c)
+
 	params = find_source_objects_and_variables(session)
-	print(params)
+	# for p in params:
+	# 	print(p)
+
 	paths = find_pdg_paths(session, params, calls)
 
 	if len(paths) > 0:
