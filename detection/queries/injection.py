f119ee51b5438640ce71b61cefc29ac7ca4b7a39
Mafalda Ferreira <mafalda.baptista@tecnico.ulisboa.pt>
2023-10-16 10:18:37 +0100

    Comment reconstruction query

--- detection/queries/injection.py
+++ detection/queries/injection.py
@@ -49,16 +49,16 @@ class Injection(QueryType):
 			param_name = my_utils.format_name(record["param"]["IdentifierName"])
 			source_location = json.loads(source_cfg["Location"])
 			sink_location = json.loads(record["sink_cfg"]["Location"])
-			tainted_params, params_types = self.reconstruct_attacker_controlled_data(session, record, attacker_controlled_data, config) 
+			#tainted_params, params_types = self.reconstruct_attacker_controlled_data(session, record, attacker_controlled_data, config)
 
 			vuln_path = {
 				"vuln_type": my_utils.get_injection_type(sink_name, config),
 				"source":  source_cfg["IdentifierName"] if source_ast["Type"] == "FunctionExpression" or source_ast["Type"] == "ArrowFunctionExpression" else param_name,
 				"source_lineno": source_location["start"]["line"],
 				"sink": sink_name,
-				"sink_lineno": sink_location["start"]["line"],
-				"tainted_params": tainted_params,
-				"params_types": params_types,
+				"sink_lineno": sink_location["start"]["line"]#,
+				#"tainted_params": tainted_params,
+				#"params_types": params_types,
 			}
 			if vuln_path not in vuln_paths:
 				vuln_paths.append(vuln_path)
