8d94f5b3e8b5cf997175fa71011538ad9f842846
Miguel Coimbra <mcoimbra@users.noreply.github.com>
2023-11-01 01:56:33 +0000

    Debugging.

--- test.py
+++ test.py
@@ -2227,7 +2227,8 @@ def generate_dataset_index(input_path: str, index_name: str = 'dataset-index.txt
             for l in lines:
                 full_path: str = f"{input_path}{os.path.sep}{l.strip()}"
                 specific_packages.append(full_path)
-            print(f'[INFO][{THIS_SCRIPT_NAME}] - Flag --specific-package-list specified {len(package_paths)} packages.')
+
+            print(f'[INFO][{THIS_SCRIPT_NAME}] - Flag --specific-package-list specified {len(specific_packages)} packages.')
         index_name = index_name[: index_name.rfind('.')] + '-' + str(len(specific_packages)) + '.txt'
 
     index_file_path: str = os.path.join(input_path, index_name)
