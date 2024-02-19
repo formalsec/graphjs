3b1e8001ff7a92fadd7f82ef123f61f18feaa04d
Mafalda Ferreira <mafalda.baptista@tecnico.ulisboa.pt>
2024-02-19 13:15:28 +0000

    Improve installation steps

--- setup.sh
+++ setup.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 
-pip3 install detection/requirements.txt
-cd parser && npm install
+# Check if node is installed
+if which node > /dev/null; then
+    echo "[INFO] Node is installed with version: $(node -v)."
+else
+    echo "[ERROR] Node is not installed. Please install node first."
+    exit 1
+fi
 
-cd ../instrumentation2 && opam install .
+# Check if python3 is installed
+if which python3 > /dev/null; then
+    echo "[INFO] Python3 is installed."
+else
+    echo "[ERROR] Python3 is not installed. Please install python3 first."
+    exit 1
+fi
+
+
+# Install npm dependencies
+(cd parser && npm install)
+
+# Python dependencies
+pip3 install -r ./requirements.txt
+
